af23c29a0e0e7d101b40fd9d61c28bbcfb7536b7
Diego Almeida <dyhalmeida@gmail.com>
2020-07-12 14:25:47 -0300

    added test step refactor

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,9 +1,9 @@
 const { Router } = require('express');
 const ProductsController = require('../controllers/products');
+const Product = require('../models/products');
 
 const router = Router();
-
-const productsController = new ProductsController();
-router.get('/', productsController.index);
+const productsController = new ProductsController(Product);
+router.get('/', (req, res) => productsController.index(req, res));
 
 module.exports = router;
--- test/integration/routes/products.spec.js
+++ test/integration/routes/products.spec.js
@@ -1,3 +1,5 @@
+const Product = require("../../../src/models/products");
+
 describe('Routes: Products', () =>{
 
     let request;
@@ -18,12 +20,31 @@ describe('Routes: Products', () =>{
         price: 100
     };
 
+    const expectedProduct = {
+        __v: 0,
+        _id: '56cb91bdc3464f14678934ca',
+        name: 'Default name',
+        description: 'Default description',
+        price: 100
+    }
+
+    beforeEach(async () => {
+        await Product.deleteMany();
+        const product = await Product(defaultProduct);
+        product._id = '56cb91bdc3464f14678934ca';
+        return await product.save();
+    });
+
+    afterEach(async () => {
+        await Product.deleteMany();
+    })
+
     describe('GET /products', () => {
         it('should return a list of products', done => {
 
             request.get('/products').end((err, res) => {
-                const [product] = res.body;
-                expect(product).to.eql(defaultProduct);
+                console.log(res.body)
+                expect(res.body).to.eql([expectedProduct]);
                 done(err);
             });
 
